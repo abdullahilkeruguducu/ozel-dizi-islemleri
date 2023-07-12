49f7dd65a00da20e197efe09470a5bb8af278895
Abdullah Ilker UGUDUCU <abdullahilkeruguducu@gmail.com>
2023-07-12 17:37:07 +0300

    Butun fonksiyonlar tamamlandi.

--- index.ts
+++ index.ts
@@ -8,6 +8,13 @@ function uniqueElements(arr: number[]) {
   return unique;
 }
 
+function commonElements(arr1: number[], arr2: number[]) {
+  const common = arr1.filter((value) => arr2.includes(value));
+  return common;
+}
+
 console.log(rotateArray([1, 2, 3, 4, 5], 2));
 
 console.log(uniqueElements([1, 2, 2, 3, 4, 4, 5]));
+
+console.log(commonElements([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]));
